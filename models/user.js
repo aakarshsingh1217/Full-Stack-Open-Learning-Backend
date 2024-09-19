d56468de48e97529377a937d3c2703951050925d
Aakarsh Singh <aaks15721@gmail.com>
2024-09-19 20:42:00 +0930

    finished learning token authentication and implementing functionality for user login and management

--- models/user.js
+++ models/user.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
-  passwordHash: String,
-  notes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Note'
-    }
-  ],
+const userSchema = mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true // this ensures the uniqueness of username
+    },
+    name: String,
+    passwordHash: String,
+    notes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Note'
+      }
+    ],
 })
 
 userSchema.set('toJSON', {
